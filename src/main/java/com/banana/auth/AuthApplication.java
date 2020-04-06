225d954b1344e2cbc2a89c6ff5848ecaed5e6987
li.yang <534889865@qq.com>
2020-04-06 15:08:51 +0800

    创建mapper包

--- src/main/java/com/banana/auth/AuthApplication.java
+++ src/main/java/com/banana/auth/AuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan
+@MapperScan("com.banana.auth.mapper")
 public class AuthApplication {
 
     public static void main(String[] args) {
